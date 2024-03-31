d3ec520af9a66bfaf9d1971f236503a179515642
Redot <coderedot@outlook.com>
2024-03-31 12:54:58 +0300

    - integrated ignore short cuts - fixed counting to the -1 - Standardized imports - improved import structure

--- src/main/java/com/Redot/Main.java
+++ src/main/java/com/Redot/Main.java
@@ -1,51 +1,152 @@
 package com.Redot;
 
-import java.io.IOException;
-import java.io.File;
-import java.io.FileFilter;
-import java.util.ArrayList;
-
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
+import javafx.util.converter.IntegerStringConverter;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.DirectoryFileFilter;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.function.UnaryOperator;
+import java.util.regex.Pattern;
 
 
-public class Main {
+public class Main extends Application {
+    private TextField sourceDirectoryField;
+    private TextField destinationDirectoryField;
+    private TextField folderItemLimiter;
 
-    public static void main(String[] args) throws IOException {
+    public void start(Stage primaryStage) {
+
+        // create grid pane to hold UI elements
+        GridPane gridPane = new GridPane();
+        gridPane.setAlignment(Pos.CENTER);
+        gridPane.setHgap(10);
+        gridPane.setVgap(10);
+        gridPane.setPadding(new Insets(25, 25, 25, 25));
+
+        // source directory selector
+        Button sourceDirectoryButton = new Button("Select...");
+        Label sourceDirectoryLabel = new Label("Source Directory:");
+        sourceDirectoryField = new TextField();
+        sourceDirectoryButton.setOnAction(e -> {
+            DirectoryChooser directoryChooser = new DirectoryChooser();
+            File selectedDirectory = directoryChooser.showDialog(primaryStage);
+            if (selectedDirectory != null) {
+                sourceDirectoryField.setText(selectedDirectory.getAbsolutePath());
+            }
+        });
+
+        // destination directory selector
+        Button targetDirectoryButton = new Button("Select...");
+        Label targetDirectoryLabel = new Label("Destination Directory:");
+        destinationDirectoryField = new TextField();
+
+        targetDirectoryButton.setOnAction(e -> {
+            DirectoryChooser directoryChooser = new DirectoryChooser();
+            File selectedDirectory = directoryChooser.showDialog(primaryStage);
+            if (selectedDirectory != null) {
+                String destinationPath = selectedDirectory.getAbsolutePath();
+                if (!destinationPath.equals(sourceDirectoryField.getText()) &&
+                        !destinationPath.startsWith(sourceDirectoryField.getText() + File.separator)) {
+                    destinationDirectoryField.setText(destinationPath);
+                } else {
+                    // Display an error message or handle the case where the destination is not valid
+                    System.out.println("Invalid destination directory. Choose a different directory.");
+                }
+            }
+        });
+
+        // folder items limitation
+        Label folderItemLimiterLabel = new Label("Items in folder limit (number):");
+        UnaryOperator<TextFormatter.Change> integerFilter = change -> {
+            Pattern validInteger = Pattern.compile("\\d*");
+
+            return validInteger.matcher(change.getControlNewText()).matches() ? change : null;
+        };
+
+        folderItemLimiter = new TextField();
+        folderItemLimiter.setTextFormatter(new TextFormatter<>(new IntegerStringConverter(), 25, integerFilter));
 
 
-//        File directory = new File("C:\\Users\\Redot\\Desktop");
-//
-//        // Create a file filter to exclude shortcuts
-//        FileFilter fileFilter = new FileFilter() {
-//            @Override
-//            public boolean accept(File file) {
-//                return !file.getName().toLowerCase().endsWith(".lnk") && !file.getName().equals("desktop.ini");
-//            }
-//        };
-//
-//        // Get a list of files in the directory, excluding shortcuts
-//        File[] files = directory.listFiles(fileFilter);
-//
-//        // Create an ArrayList to store the file names
-//        ArrayList<String> fileList = new ArrayList<>();
-//
-//        // Process the files...
-//        if (files != null) {
-//            for (File file : files) {
-//                fileList.add(file.getName());
-//            }
-//        } else {
-//            System.out.println("Directory not found or not accessible.");
-//        }
+        // ignore shortcuts check box
+        CheckBox ignoreShortcutsCheckBox = new CheckBox("Ignore Shortcuts");
 
-        // Print the list of file names
-//        System.out.println(fileList);
+        // start button
+        Button startButton = new Button("Start");
+        startButton.setOnAction(e -> {
+            // Do file copying here
+            // destination naming
+            LocalDateTime myDateObj = LocalDateTime.now();
+            DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("yyyy MMM");
+            String destination = destinationDirectoryField.getText() + File.separator + (myDateObj.format(myFormatObj));
 
+            System.out.println(sourceDirectoryField.getText()); // DELETE ME!!!
 
-        /////////////////
-        DirectorySelectorApp dirSA = new DirectorySelectorApp();
-        dirSA.starter();
-        /////////////////
+            // Checks if source exists
+            if (!DirectoryFileFilter.DIRECTORY.accept(new File(sourceDirectoryField.getText()))) {
+                System.out.println("Source does not exist \nExiting...");
+                System.exit(0);
+            }
 
+            // Checks if destination exists
+            if (!DirectoryFileFilter.DIRECTORY.accept(new File(destinationDirectoryField.getText()))) {
+                System.out.println("Creating the destination...");
+                try {
+                    FileUtils.forceMkdir(new File(destinationDirectoryField.getText()));
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+            try {
+                CopyMachine.copyRecurseHandler(sourceDirectoryField.getText(), destination, Integer.parseInt(folderItemLimiter.getText())-1, 1, ignoreShortcutsCheckBox.isSelected());
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+
+            // End file copying here
+        });
+
+        // add UI elements to grid pane
+        gridPane.add(sourceDirectoryLabel, 0, 0);
+        gridPane.add(sourceDirectoryField, 1, 0);
+        gridPane.add(sourceDirectoryButton, 2, 0);
+        gridPane.add(targetDirectoryLabel, 0, 1);
+        gridPane.add(destinationDirectoryField, 1, 1);
+        gridPane.add(targetDirectoryButton, 2, 1);
+        gridPane.add(folderItemLimiterLabel, 0, 2);
+        gridPane.add(folderItemLimiter, 1, 2);
+        gridPane.add(ignoreShortcutsCheckBox, 2, 2);
+
+        gridPane.add(startButton, 1, 3);
+
+        // create vertical box to hold grid pane and log label
+        VBox vbox = new VBox();
+        vbox.setSpacing(10);
+        vbox.setPadding(new Insets(10, 10, 10, 10));
+        vbox.getChildren().addAll(gridPane);
+        Scene scene = new Scene(vbox);
+        primaryStage.setScene(scene);
+
+        // show the window
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+
+        System.out.print("mew");
+
+        launch(args);
     }
 }
